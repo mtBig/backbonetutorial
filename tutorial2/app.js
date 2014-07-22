9ef2920127d42c4213b09e7a018f72dfaa3f076f
sjyun <darkandark90@gmail.com>
2014-07-22 19:11:38 +0900

    'e'

--- tutorial2/app.js
+++ tutorial2/app.js
@@ -3,7 +3,7 @@
 var appleData = [
     {
         name:'test1',
-        url:'img/test1.jpg'
+        url:'img/img1.jpg'
     },
     {
         name:'test2',
@@ -16,18 +16,47 @@ var app;
 var router = Backbone.Router.extend({
     routes:{
         '': 'home',
-        'apples/:appleName': 'loadApple'
+        'foods/:foodName': 'loadApple'
     },
     initialize: function(){
         var apples = new Apples();
         apples.reset(appleData);
         this.homeView = new homeView( {collection: apples});
-        this.appleView = new appleView({collection:apples});
+        this.appView = new appView({collection:apples});
     },
     home:function(){
         this.homeView.render();
     },
-    loadApple: function(appleName){
-        this.appleView.render(appleName);
+    loadApple: function(foodName){
+        this.appView.render(foodName);
     }
+});
+
+var homeView = Backbone.View.extend({
+    el:'body',
+    template: _.template('data: <%= data %>' ),
+    render:function(){
+        this.$el.html( this.template( {data: JSON.stringify(this.collection.models)}));
+    }
+});
+
+var Apples = Backbone.Collection.extend({
+
+});
+
+var appView = Backbone.View.extend({
+    template: _.template('<figure>\
+                              <img src="<%= attributes.url%>"/>\
+                              <figcaption><%= attributes.name %></figcaption>\
+                            </figure>'),
+    render: function(foodName){
+        var appleModel = this.collection.where({name:foodName})[0];
+        var appleHtml = this.template(appleModel);
+        $('body').html(appleHtml);
+    }
+});
+
+$(document).ready( function(){
+    app = new router;
+    Backbone.history.start();
 });
