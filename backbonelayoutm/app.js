7e6be363eb70f1f9d095fed4ac534cfddde50f97
ysj <darkandark90@gmail.com>
2014-07-22 14:20:54 +0900

    'layout'

--- backbonelayoutm/app.js
+++ backbonelayoutm/app.js
@@ -1,6 +1,37 @@
+var View  = Backbone.Layout.extend({
+    template:'#view',
+    events:{
+        "click": "wrapElement" ,
+        "mouseenter": "insertElement",
+        "mouseleave": "removeElement"
+    },
+    wrapElement: function() {
+        this.$el.wrap("<b>");
+    },
+    insertElement: function() {
+        this.insertView(new DisplayView()).render();
+    },
+
+    removeElement: function() {
+        // Removes the inserted DisplayView.
+        this.removeView("");
+    }
+
+});
+
+var DisplayView = Backbone.View.extend({
+    manage: true,
+
+    template: "#display"
+});
+
+
 var layout = new Backbone.Layout({
     el: '.main',
-    template:'#layout'
+    template:'#layout'   ,
+    views:{
+        'p': new View()
+    }
 });
 
 layout.render();
